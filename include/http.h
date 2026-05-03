9b12d2ea955e74d30f9ed6f80af4dc2c76c00d14
Yusuf Tarlan <tarlanyusuf@outlook.com>
2026-05-03 14:24:43 +0300

    file gönderme işlemi tamamlandı

--- include/http.h
+++ include/http.h
@@ -2,20 +2,34 @@
 #ifndef HTTP_H
 #define HTTP_H
 #include "base.h" //dasda
+#include <sys/socket.h>
 typedef struct
 {
     char method[16];
     char path[256];
     char version[16];
+    char *body;
 } HttpRequest;
 
 typedef struct
 {
-    char response[16];
-    char path[256];
-    char version[16];
+    char statusLine[32];
+    const char *MIMEtype;
+    char headers[300];
+    long fileSize;
 } HttpResponse;
 
+extern const char *httpOKStatus;
+extern char *httpNotFoundResponse;
+extern const char *httpServerErrorStatus;
+
+void initHttpNotFoundHeaders();
+void initHttpServerErrorHeaders();
+int initHttpNotFoundResponse();
+int sendHttpResponseHeader(int client_fd, HttpResponse *httpResponse);
+int prepHttpResponseHeader(HttpResponse *httpResponse);
 int parse_http_request(const char *buffer, HttpRequest *request);
+int send404HttpResponse(int client_fd);
+int sendFileContent(int client_fd, FILE *file);
 
 #endif
